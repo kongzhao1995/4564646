a6066c56ce405995ce7c1b8649444418c7efe849
Your Name <you@example.com>
2020-05-22 13:57:01 +0800

    项目结构完成

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-    <div>第一个项目开始了</div>
+    <el-container>
+      <el-aside width="200px" style="background-color: blue;height:100%">Aside</el-aside>
+      <el-container>
+        <el-header height='80px' style="background-color: red">Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
     <router-view/>
   </div>
 </template>
@@ -12,12 +18,17 @@ export default {
 </script>
 
 <style>
+body,html {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  width: 100%;
+  height: 100%;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+}
+.el-container {
+  height: 100%;
 }
 </style>
