bbc3a159ed94238f5f52309db6c210fe55335dd9
Your Name <you@example.com>
2020-05-25 14:18:14 +0800

    左侧导航完成

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,19 @@
       <el-aside width="200px" style="background-color: #000;height:100%">
         <div class="logo"><img src="./assets/logo.jpeg" alt=""></div>
         <el-menu
+          :router="true"
           default-active=""
           class="el-menu-vertical-demo"
           background-color="#545c64"
           text-color="#fff"
+          style="border-right: none"
           active-text-color="orange">
-          <el-submenu index="" v-for="item in list" :key='item.id'>
+          <el-submenu :index="item.id" v-for="item in list" :key='item.id'>
             <template slot="title">
               <i class="el-icon-menu"></i>
               <span>{{item.name}}</span>
             </template>
-              <el-menu-item @click="open(item2.path)" :index="item2.path" style="text-align: center;" v-for="item2 in item.data" :key='item2.name'>{{item2.name}}</el-menu-item>
+              <el-menu-item :index="item2.path" style="text-align: center;" v-for="item2 in item.data" :key='item2.name'>{{item2.name}}</el-menu-item>
           </el-submenu>
         </el-menu>
       </el-aside>
@@ -32,10 +34,10 @@ export default {
   data() {
     return {
       list: [
-        {name: '系统首页', id : '1',data: [{name: '消防首页'}]},
-        {name: '告警反馈', id : '2',data: [{name: '告警统计'}]},
+        {name: '系统首页', id : '1',data: [{name: '消防首页',path: '/system'}]},
+        {name: '告警反馈', id : '2',data: [{name: '告警统计',path: '/firerAlarm'}]},
         {name: '隐患反馈', id : '3',data: [{name: '隐患统计',path: '/dangerAlarm'}]},
-        {name: '角色查询', id : '4',data: [{name: '用户管理'}]}
+        {name: '角色查询', id : '4',data: [{name: '用户管理',path: '/user'}]}
       ]
     }
   },
--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,16 @@ Vue.use(Router)
 
 const dangerAlarm = () => import('../view/danger/dangerAlarm.vue')
 const firerAlarm = () => import('../view/fire/fireAlarm.vue')
+const user = () => import('../view/user/userManagement.vue')
+const system = () => import('../view/system/firstpage.vue')
 // console.log(dangerAlarm)
 
 export default new Router({
   routes: [
     {path: '/', redirect: '/dangerAlarm'},
-    {path: '/dangerAlarm', component: dangerAlarm}
+    {path: '/dangerAlarm', component: dangerAlarm},
+    {path: '/firerAlarm', component: firerAlarm},
+    {path: '/user', component: user},
+    {path: '/system', component: system}
   ]
 })
