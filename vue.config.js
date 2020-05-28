03972f86629b68ff190748edc9ef88cb25b55ea2
Your Name <you@example.com>
2020-05-28 21:43:10 +0800

    提交config文件

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,42 @@
+// module.exports = {
+//    devServer: {
+//      proxy: {
+//         '/': {
+//           target: 'https://cloudfire.citylink.hzcloudtown.com/',
+//           port: '8080',
+//           changeOrigin: true
+//         }
+//      }
+//    }
+// }
 module.exports = {
-  
-}
+  publicPath: process.env.NODE_ENV === "production" ? "/be/" : "./",
+  // assetsDir: process.env.NODE_ENV === "production" ? "/be/" : "./"
+  devServer: {
+    proxy: {
+      "/": {
+        // 目标 API 地址
+        target: "https://cloudfire.citylink.hzcloudtown.com",
+        host: "0.0.0.0",
+        port: 8080,
+        // 如果要代理 websockets
+        ws: false,
+        // 将主机标头的原点更改为目标URL
+        changeOrigin: true
+      }
+    }
+    // 关闭eslint
+    // overlay: {
+    //   warnings: false,
+    //   errors: false
+    // },
+    // lintOnSave: false
+
+    // }
+    // configureWebpack: {
+    //   plugins: [
+    //     new VuetifyLoaderPlugin()
+    //   ]
+    // }
+  }
+};
